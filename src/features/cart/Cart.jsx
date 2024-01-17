3d28a7f5d6a9d9fac8be5042561c3f7e092790a5
Vinod Jaggavarapu <jaggavarapu.sa@northeastern.edu>
2024-01-17 18:48:34 -0500

    Styling Using Tailwind

---
+++ src/features/cart/Cart.jsx
@@ -0,0 +1,64 @@
+// import { Link } from "react-router-dom";
+import LinkButton from "../../ui/LinkButton";
+import Button from "../../ui/Button";
+import CartItem from "./CartItem";
+
+const fakeCart = [
+  {
+    pizzaId: 12,
+    name: "Mediterranean",
+    quantity: 2,
+    unitPrice: 16,
+    totalPrice: 32,
+  },
+  {
+    pizzaId: 6,
+    name: "Vegetale",
+    quantity: 1,
+    unitPrice: 13,
+    totalPrice: 13,
+  },
+  {
+    pizzaId: 11,
+    name: "Spinach and Mushroom",
+    quantity: 1,
+    unitPrice: 15,
+    totalPrice: 15,
+  },
+];
+
+function Cart() {
+  const cart = fakeCart;
+  console.log(cart);
+
+  return (
+    <div className="px-4 py-3">
+      {/* <Link
+        to="/menu"
+        className="text-sm text-blue-500 hover:text-blue-600 hover:underline"
+      >
+        &larr; Back to menu
+      </Link> */}
+      <LinkButton to="/menu"> &larr; Back to menu</LinkButton>
+
+      <h2 className="mt-7 text-xl font-semibold">Your cart, %NAME%</h2>
+
+      <ul className="mt-3 divide-y divide-stone-200 border-b">
+        {cart.map((item) => {
+          return <CartItem item={item} key={item.key} />;
+        })}
+      </ul>
+
+      <div className="mt-6 space-x-2">
+        <Button to="/order/new" type="primary">
+          Order pizzas
+        </Button>
+        {/* <Link to="/order/new"></Link> */}
+        {/* <button>Clear cart</button> */}
+        <Button type="secondary">Clear cart</Button>
+      </div>
+    </div>
+  );
+}
+
+export default Cart;
